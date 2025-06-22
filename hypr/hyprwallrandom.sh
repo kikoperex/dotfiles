5a7f7f390956f0d4e66168d7e1c4d889d61c50d2
kikoperex <iblamekiko@gmail.com>
2025-06-22 02:25:36 -0600

    pequeñas mejoras

--- hypr/hyprwallrandom.sh
+++ hypr/hyprwallrandom.sh
@@ -7,5 +7,5 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 # Apply the selected wallpaper
-wal -i "$WALLPAPER" --cols16 --saturate 0.6 -b 000000 
+wal -i "$WALLPAPER" --cols16 --saturate 0.6 -b 000000 -n
 hyprctl hyprpaper reload ,"$WALLPAPER"
