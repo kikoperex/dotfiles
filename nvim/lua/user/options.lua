e3589c4e07d35e276138572d93ffbdc7d1771628
kikoperex <iblamekiko@gmail.com>
2025-10-25 04:05:47 -0600

    cambios al entorno

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -7,11 +7,16 @@ opt.cursorline = false
 opt.ignorecase = true
 opt.smartcase = true
 opt.hlsearch = false
+opt.incsearch = true
 opt.swapfile = false
 opt.undofile = true
 opt.autoread = true
 opt.scrolloff = 8
 opt.colorcolumn = "80"
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 opt.smartindent = true
 
 -- Enable filetype plugins and indenting
